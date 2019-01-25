99fe5a0b8281f481425093ea1b84cd619a46ebab
filwie <filip.wiechec@gmail.com>
2019-01-25 23:35:58 +0100

    Add more test cases.

--- test_colorscheme.py
+++ test_colorscheme.py
@@ -21,3 +21,41 @@ def test_color_init_correct(test_input):
 def test_color_init_incorrect(test_input):
     with pytest.raises(TypeError):
         colorscheme.Color(test_input)
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ([123, 123, 123], True),
+    ((12, 12, 12), True),
+    ('#123123', False),
+    ('#eee123', False),
+])
+def test_is_rgb(test_input, expected):
+    assert colorscheme.Color.is_rgb(test_input) is expected
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ([123, 123, 123], False),
+    ((12, 12, 12), False),
+    ('#123123', True),
+    ('#eee123', True),
+])
+def test_is_hex(test_input, expected):
+    assert colorscheme.Color.is_hex(test_input) is expected
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ((123, 123, 123), (123, 123, 123)),
+    ((12, 12, 12), (12, 12, 12)),
+    ('#123123', (18, 49, 35)),
+    ('#eee123', (238, 225, 35)),
+])
+def test_conversion_to_rgb(test_input, expected):
+    assert colorscheme.Color(test_input).rgb == expected
+
+
+'''
+    ((123, 123, 123), '#7b7b77b'),
+    ((12, 12, 12), '#0c0c0c'),
+    ('#123123', (18, 49, 35)),
+    ('#eee123', (238, 225, 35)),
+    '''
